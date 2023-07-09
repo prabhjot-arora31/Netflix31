4c194c3d64f1008c857bf78f09b62b1de98e44d2
prabhjot-arora31 <83449967+prabhjot-arora31@users.noreply.github.com>
2023-07-09 15:31:01 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div
+      style={{
+        // width: "100%",
+        backgroundColor: "black",
+        color: "white",
+        padding: "0.7rem",
+        position: "relative",
+        bottom: "-4.6rem",
+        textAlign: "center",
+      }}
+    >
+      Data Reference: TMDB API.
+    </div>
+  );
+}
+
+export default Footer;
